9414146a480f3d1dfb6768a11561de9ddc7aede2
I066841 <a.kulshreshtha@sap.com>
2019-01-21 11:42:07 +0530

    changes

--- src/BigDecimalCheck.java
+++ src/BigDecimalCheck.java
@@ -4,12 +4,19 @@ import java.math.RoundingMode;
 public class BigDecimalCheck {
 
 	public static void main(String[] args) {
+		Double value1 = new Double(12.5611111);
 		BigDecimal tax = new BigDecimal(12.4554);
-        
 		tax = tax.setScale(2, RoundingMode.HALF_UP);
-		
 		System.out.println(tax);
+
+		double dvalue1 = value1.doubleValue();
+		BigDecimal dvalueTest = new BigDecimal(dvalue1);
+		System.out.println(dvalueTest);
+
+		System.out.println(dvalueTest.setScale(2, BigDecimal.ROUND_HALF_UP));
 		
+		
+
 	}
 
 }
